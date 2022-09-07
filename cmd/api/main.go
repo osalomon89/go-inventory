b693fd294116daa12d752d5d7a9cff3783799147
daruiz1337 <david.ruiz@mercadolibre.com>
2022-09-07 14:36:01 -0300

    server refactor

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,39 +1,22 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
-	"net/http"
 
-	"github.com/gorilla/mux"
-	"github.com/osalomon89/go-inventory/internal/repositories"
+	"github.com/osalomon89/go-inventory/internal/server"
 )
 
 func main() {
-	router := mux.NewRouter()
 
 	const port string = ":8888"
+	httpServer := server.NewHTTPRouter(port)
 
-	router.HandleFunc("/ping", ping).Methods("GET")
-	router.HandleFunc("/books/{id}", repositories.GetBookByID).Methods("GET")
-	router.HandleFunc("/books", repositories.GetBooks).Methods("GET")
-	router.HandleFunc("/books", repositories.PostBook).Methods("POST")
-	router.HandleFunc("/books/{id}", repositories.PutBook).Methods("PUT")
-	router.HandleFunc("/books/{id}", repositories.DeleteBook).Methods("DELETE")
+	router := httpServer.SetupRouter()
 
 	log.Println("Server listening on port", port)
 
-	err := http.ListenAndServe(port, router)
+	err := httpServer.Run(router)
 	if err != nil {
 		log.Fatalln(err)
 	}
-}
-func ping(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-
-	json.NewEncoder(w).Encode(repositories.ResponseInfo{
-		Status: http.StatusOK,
-		Data:   "pong",
-	})
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,49 @@
 package server
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/osalomon89/go-inventory/internal/repositories"
+)
+
+type HTTPRouter interface {
+	SetupRouter() *mux.Router
+	Run(router http.Handler) error
+}
+
+type httpRouter struct {
+	port string
+}
+
+func NewHTTPRouter(port string) HTTPRouter {
+	return &httpRouter{
+		port: port}
+}
+
+func (r *httpRouter) SetupRouter() *mux.Router {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/ping", ping).Methods("GET")
+	router.HandleFunc("/books/{id}", repositories.GetBookByID).Methods("GET")
+	router.HandleFunc("/books", repositories.GetBooks).Methods("GET")
+	router.HandleFunc("/books", repositories.PostBook).Methods("POST")
+	router.HandleFunc("/books/{id}", repositories.PutBook).Methods("PUT")
+	router.HandleFunc("/books/{id}", repositories.DeleteBook).Methods("DELETE")
+
+	return router
+}
+
+func (r *httpRouter) Run(router http.Handler) error {
+	return http.ListenAndServe(r.port, router)
+}
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(repositories.ResponseInfo{
+		Status: http.StatusOK,
+		Data:   "pong",
+	})
+}
